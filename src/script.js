1574ff7ac892a6f1ad28484b302ad6f89681bbfd
Mixailcode <vladysislavdobriy@gmail.com>
2024-07-22 18:25:52 +0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -7,3 +7,29 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        var audio = document.getElementById('backgroundMusic');
+		// Set the volume to 50%
+		audio.volume = 0.5;
+        audio.play().catch(function(error) {
+            console.log('Error playing audio: ', error);
+        });
+    }, 5000); // 5000 milliseconds = 5 seconds
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var playButton = document.getElementById('playButton');
+    var audio = document.getElementById('backgroundMusic');
+    
+    // Set the volume to 50%
+    audio.volume = 0.2;
+
+    playButton.addEventListener('click', function() {
+        setTimeout(function() {
+            audio.play().catch(function(error) {
+                console.log('Error playing audio: ', error);
+            });
+        }, 5000); // 5000 milliseconds = 5 seconds
+    });
+});
